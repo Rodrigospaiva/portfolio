07ab7184977bb2718758645bfe15e0ee569aa1ed
Rodrigospaiva <desenvolvimento.rsp@gmail.com>
2021-09-24 20:27:53 -0300

    Publicação

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$('.slider-skills').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
--- js/smtp.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    host: "smtp.gmail.com",
-    port: 587,
-    user: "engenharia.rsilva@gmail.com",
-    pass: "44809090"
-}
